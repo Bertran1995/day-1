72b300f6f7fccd23dcfa0434413bafc9947789e9
Bertran1995 <67956653+Bertran1995@users.noreply.github.com>
2020-07-08 14:29:51 +0200

    Create print_groups.sh
    Changed file name

---
+++ ex04/print_groups.sh
@@ -0,0 +1,14 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    print_groups.sh                                    :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: bstrydom <marvin@42.fr>                    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2020/07/08 09:16:15 by bstrydom          #+#    #+#              #
+#    Updated: 2020/07/08 10:31:59 by bstrydom         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+groups $FT_USER | tr ' ' ',' # tr (transliterate) allows for the conversion of text from one character to another. 
+
